c3a7f86bb26c095d72a9689945f2e26b0970977b
QuocTung95 <quoctung.haui@gmail.com>
2025-06-08 15:16:45 +0700

    dynamic api

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// import { Connection } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // for the first init db
+  //   const connection = app.get(Connection);
+  //   await connection.runMigrations();
+
   // Enable CORS
   app.enableCors({
-    origin: ['https://kyc-seven-xi.vercel.app'],
+    origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -22,7 +22,15 @@ export default defineConfig({
     ],
   },
   server: {
+    host: true,
     port: 3000,
-    open: true,
+    open: false,
+    proxy: {
+      "/api": {
+        target: "http://server:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
